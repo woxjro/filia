97c08eb2aaf2a35d535ce8d5eff794f50f77570b
woxjro <usuzawa.yoshi.55z@st.kyoto-u.ac.jp>
2024-05-17 23:33:30 +0900

    feat(michelson): add pair

--- lib/Michelson/MichelsonDialect.cpp
+++ lib/Michelson/MichelsonDialect.cpp
@@ -69,6 +69,56 @@ namespace detail {
       llvm::ArrayRef<mlir::Type> elementType;
     };
 
+    struct PairTypeStorage : public mlir::TypeStorage {
+      /// The `KeyTy` is a required type that provides an interface for the storage
+      /// instance. This type will be used when uniquing an instance of the type
+      /// storage. For our struct type, we will unique each instance structurally on
+      /// the elements that it contains.
+      using KeyTy = llvm::ArrayRef<mlir::Type>;
+
+      /// A constructor for the type storage instance.
+      PairTypeStorage(llvm::ArrayRef<mlir::Type> elementTypes)
+          : elementTypes(elementTypes) {}
+
+      /// Define the comparison function for the key type with the current storage
+      /// instance. This is used when constructing a new instance to ensure that we
+      /// haven't already uniqued an instance of the given key.
+      bool operator==(const KeyTy &key) const { return key == elementTypes; }
+
+      /// Define a hash function for the key type. This is used when uniquing
+      /// instances of the storage.
+      /// Note: This method isn't necessary as both llvm::ArrayRef and mlir::Type
+      /// have hash functions available, so we could just omit this entirely.
+      static llvm::hash_code hashKey(const KeyTy &key) {
+        return llvm::hash_value(key);
+      }
+
+      /// Define a construction function for the key type from a set of parameters.
+      /// These parameters will be provided when constructing the storage instance
+      /// itself, see the `PairType::get` method further below.
+      /// Note: This method isn't necessary because KeyTy can be directly
+      /// constructed with the given parameters.
+      static KeyTy getKey(llvm::ArrayRef<mlir::Type> elementTypes) {
+        return KeyTy(elementTypes);
+      }
+
+      /// Define a construction method for creating a new instance of this storage.
+      /// This method takes an instance of a storage allocator, and an instance of a
+      /// `KeyTy`. The given allocator must be used for *all* necessary dynamic
+      /// allocations used to create the type storage and its internal.
+      static PairTypeStorage *construct(mlir::TypeStorageAllocator &allocator,
+                                          const KeyTy &key) {
+        // Copy the elements from the provided `KeyTy` into the allocator.
+        llvm::ArrayRef<mlir::Type> elementTypes = allocator.copyInto(key);
+
+        // Allocate the storage instance and construct it.
+        return new (allocator.allocate<PairTypeStorage>())
+            PairTypeStorage(elementTypes);
+      }
+
+      /// The following field contains the element types of the struct.
+      llvm::ArrayRef<mlir::Type> elementTypes;
+    };
 
     struct StructTypeStorage : public mlir::TypeStorage {
       /// The `KeyTy` is a required type that provides an interface for the storage
--- test/examples/michelson.mlir
+++ test/examples/michelson.mlir
@@ -4,8 +4,10 @@ module {
     // CHECK-LABEL: func.func @smart_contract()
     func.func @smart_contract() {
         // CHECK: %{{.*}} = michelson.get_amount: !michelson.mutez
-        %res = michelson.get_amount: !michelson.mutez
-        %list = michelson.make_list: !michelson.list<!michelson.mutez>
+        %amount1 = michelson.get_amount(): !michelson.mutez
+        %amount2 = michelson.get_amount(): !michelson.mutez
+        %list = michelson.make_list(): !michelson.list<!michelson.operation>
+        %pair = michelson.make_pair %amount1, %amount2: !michelson.pair<!michelson.mutez, !michelson.mutez>
         return
     }
 }
